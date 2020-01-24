35eb7b20c4c0580b40042350fdce3b3c5b3bf743
NaveenDelegate <naveenprasad43@gmail.com>
2020-01-24 16:05:04 +0530

    Localization Initial Commit.
    This is to check the Trial version usage

---
+++ Localize/Localize/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Localize
+//
+//  Created by Naveen on 21/01/20.
+//  Copyright © 2020 Local. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
